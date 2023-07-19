a7425ea4bc12b0fd6381268c75d522e3de54173b
chiadikaobixo <chiadikaobixo@gmail.com>
2023-07-19 12:20:11 -0500

    authentication-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { getEnvironment } from './configs/env.configs';
 import { HealthModule } from './modules/common/health/health.module';
+import { AuthenticationServiceModule } from './modules/core/authenticationservice/authenticationservice.module';
 
 @Module({
   imports: [
     HealthModule,
+    AuthenticationServiceModule,
     ConfigModule.forRoot({
       load: getEnvironment(),
       isGlobal: true,
