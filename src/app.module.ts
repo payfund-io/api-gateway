0d0f1ed88120375e0f1d1c212c891660d4f90a8e
chiadikaobixo <chiadikaobixo@gmail.com>
2023-07-22 07:50:02 -0500

    gateway-update

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,12 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { getEnvironment } from './configs/env.configs';
 import { HealthModule } from './modules/common/health/health.module';
-import { AuthenticationServiceModule } from './modules/core/authenticationservice/authenticationservice.module';
+import { IdentificationServiceModule } from './modules/core/identificationservice/identificationservice.module';
 
 @Module({
   imports: [
     HealthModule,
-    AuthenticationServiceModule,
+    IdentificationServiceModule,
     ConfigModule.forRoot({
       load: getEnvironment(),
       isGlobal: true,
--- src/modules/core/identificationservice/services/identificationservice.service.ts
+++ src/modules/core/identificationservice/services/identificationservice.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from "@nestjs/common";
 
 
 @Injectable()
-export class AuthenticationService{
+export class IdentificationService{
 
 }
